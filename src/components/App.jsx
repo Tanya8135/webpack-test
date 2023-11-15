9cbb9d2b747176cab4bd13411091632e69c6a998
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-11-15 20:17:12 +0200

    add swiper slider Effect coverflow

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 // Import Swiper React components
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  EffectCoverflow,
+} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
@@ -151,6 +157,81 @@ export default function App() {
           </article>
         </SwiperSlide>
       </Swiper>
+
+<div className='container'>
+      <div className="box">
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-1.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-2.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-3.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-4.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-5.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-6.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-7.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-8.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+          <SwiperSlide className='rotate_slider'>
+            <img
+              src="https://swiperjs.com/demos/images/nature-9.jpg"
+              width={100}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import './styles.css'
 
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
