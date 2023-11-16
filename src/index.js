5dac4a81627f7f3f391d67fd5add94085f8ef2e3
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-11-17 00:58:44 +0200

    back to start

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-// import _ from 'lodash'
-import './styles.css'
-// import './sass/main.scss'
-
-// function component() {
-//     const element = document.createElement('div')
-
-//     // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-//     return element
-// }
-
-// document.body.appendChild(component())
+import _ from 'lodash';
+import './styles.css';
+import './sass/main.scss';
 
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('app'))
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+function component() {
+    const element = document.createElement('div')
+
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    return element
+}
+
+document.body.appendChild(component())
+
+
+// /**
+//  * for REACT
+//  */
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./components/App.jsx";
+// import './styles.css'
+
+// const root = ReactDOM.createRoot(document.getElementById('app'))
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// )
