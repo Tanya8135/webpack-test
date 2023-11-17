25b002bde6f51e9c259a61028f2b3f8c3c424ac2
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-11-17 16:36:48 +0200

    add slick slider

---
+++ src/test.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.box-slide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+    });
+
+    let filtered = false;
+
+    $('.js-filter').on('click', function () {
+        if (filtered === false) {
+            $('.box-slide').slick('slickFilter', ':even');
+            $(this).text('Unfilter Slides');
+            filtered = true;
+        } else {
+            $('.box-slide').slick('slickUnfilter');
+            $(this).text('Filter Slides');
+            filtered = false;
+        }
+    });
+});
